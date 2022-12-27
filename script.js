e36f3f8361a92fd62936afb27bb8f5df28cab83a
nayakmadhurika <nayakmadhurika6@gmail.com>
2022-12-27 13:13:38 +0530

    added bg color

--- script.js
+++ script.js
@@ -27,3 +27,13 @@ var header = document.getElementById('main-header');
  addItem.style.color = 'green';
 
 
+//get element by class name
+var items = document.getElementsByClassName('list-group-item');
+items[1].textContent = 'hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+
+
+for(var i = 0; i < items.length; i++){
+    items[i].style.backgroundColor = '#f4f4f4';
+}
