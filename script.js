1533bc3217dad717bb7ca3f1b71df8108790d0b6
nayakmadhurika <nayakmadhurika6@gmail.com>
2022-12-27 13:00:43 +0530

    added bg color to items

--- script.js
+++ script.js
@@ -28,3 +28,12 @@ var header = document.getElementById('main-header');
 
 
 //get element by class name
+var items = document.getElementsByClassName('list-group-item');
+items[1].textContent = 'hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+
+
+for(var i = 0; i < items.length; i++){
+    items[i].style.backgroundColor = '#f4f4f4';
+}
